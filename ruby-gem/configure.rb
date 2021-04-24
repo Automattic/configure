a8f952cd7414771678bfce6c1f2419d19b237001
Jeremy Massel <jeremy.massel@automattic.com>
2021-04-23 23:47:57 -0600

    Add debug messages in development mode

--- ruby-gem/configure.rb
+++ ruby-gem/configure.rb
@@ -8,7 +8,11 @@ module Configure
 	lib_name = File.join(__dir__, "bin", "libconfigure.dylib")
 
 	if is_development_environment
+		puts "In development mode"
 		lib_name = File.join(File.dirname(File.expand_path(__dir__)), "target", "debug", "libconfigure.dylib")
+		puts "Using binary at #{lib_name}"
+		abort("The binary could not be found – run `cargo build` in the `configure` project root to build it") unless File.exist?(lib_name)
+		puts "Binary is present"
 	end
 
 	ffi_lib File.expand_path(lib_name, __dir__)
